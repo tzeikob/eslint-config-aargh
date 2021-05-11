b7ca4dbad5d0a31865ed7648602a637aaf2189cd
Iakovos Papadopoulos <iakopap@gmail.com>
2021-05-11 17:33:32 +0300

    Add parse options

--- style.js
+++ style.js
@@ -2,5 +2,12 @@ module.exports = {
   extends: [
     './rules/style'
   ].map(require.resolve),
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      impliedStrict: true
+    }
+  },
   rules: {}
 };
