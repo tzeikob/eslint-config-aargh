1d1b9a5a976f97e74c7c31d6ded09f8ed3adc309
Iakovos Papadopoulos <iakopap@gmail.com>
2021-05-22 23:06:06 +0300

    Sync es6 rules for eslint v7

--- rules/es6.js
+++ rules/es6.js
@@ -1,22 +1,53 @@
 module.exports = {
   rules: {
+    // Require braces in arrow function body
+    // https://eslint.org/docs/rules/arrow-body-style
     'arrow-body-style': ['error', 'as-needed'],
+
+    // Require parens in arrow function arguments
+    // https://eslint.org/docs/rules/arrow-parens
     'arrow-parens': ['error', 'always'],
-    'arrow-spacing': ['error', {
-      'before': true,
-      'after': true
-    }],
+
+    // Require space before/after arrow function's arrow
+    // https://eslint.org/docs/rules/arrow-spacing
+    'arrow-spacing': ['error', { before: true, after: true }],
+
+    // Verify calls of super() in constructors
+    // https://eslint.org/docs/rules/constructor-super
     'constructor-super': ['error'],
-    'generator-star-spacing': ['error', {
-      'before': true,
-      'after': false
-    }],
+
+    // Enforce spacing around the * in generator functions
+    // https://eslint.org/docs/rules/generator-star-spacing
+    'generator-star-spacing': ['error', { before: true, after: false }],
+
+    // Disallow modifying variables of class declarations
+    // https://eslint.org/docs/rules/no-class-assign
     'no-class-assign': ['error'],
+
+    // Disallow arrow functions where they could be confused with comparisons
+    // https://eslint.org/docs/rules/no-confusing-arrow
     'no-confusing-arrow': ['off'],
+
+    // Disallow modifying variables that are declared using const
+    // https://eslint.org/docs/rules/no-const-assign
     'no-const-assign': ['error'],
+
+    // Disallow duplicate name in class members
+    // https://eslint.org/docs/rules/no-dupe-class-members
     'no-dupe-class-members': ['error'],
-    'no-duplicate-imports': ['error', { 'includeExports': true }],
+
+    // Disallow duplicate imports
+    // https://eslint.org/docs/rules/no-duplicate-imports
+    'no-duplicate-imports': ['error', { includeExports: true }],
+
+    // Disallow Symbol constructor
+    // https://eslint.org/docs/rules/no-new-symbol
     'no-new-symbol': ['error'],
+
+    // Disallow specified names in exports
+    // https://eslint.org/docs/rules/no-restricted-exports
+    'no-restricted-exports': ['off'],
+
     'no-restricted-imports': ['off'],
     'no-this-before-super': ['error'],
     'no-useless-computed-key': ['error', { 'enforceForClassMembers': true }],
