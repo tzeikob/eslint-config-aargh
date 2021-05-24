508b515bdd8566a7e2144445ba513174e8bb38d0
Iakovos Papadopoulos <iakopap@gmail.com>
2021-05-24 15:00:27 +0300

    Sync es6 rules for eslint v7

--- rules/es6.js
+++ rules/es6.js
@@ -48,25 +48,88 @@ module.exports = {
     // https://eslint.org/docs/rules/no-restricted-exports
     'no-restricted-exports': ['off'],
 
+    // Disallow specific imports
+    // https://eslint.org/docs/rules/no-restricted-imports
     'no-restricted-imports': ['off'],
+
+    // Disallow use of this/super before calling super() in constructors
+    // https://eslint.org/docs/rules/no-this-before-super
     'no-this-before-super': ['error'],
-    'no-useless-computed-key': ['error', { 'enforceForClassMembers': true }],
+
+    // Disallow unnecessary computed property keys in objects and classes
+    // https://eslint.org/docs/rules/no-useless-computed-key
+    'no-useless-computed-key': ['error', { enforceForClassMembers: true }],
+
+    // Disallow unnecessary constructor
+    // https://eslint.org/docs/rules/no-useless-constructor
     'no-useless-constructor': ['error'],
-    'no-useless-rename': ['error'],
+
+    // Disallow renaming import, export, and destructured assignments to the same name
+    // https://eslint.org/docs/rules/no-useless-rename
+    'no-useless-rename': ['error', {
+      ignoreDestructuring: false,
+      ignoreImport: false,
+      ignoreExport: false
+    }],
+
+    // Require let or const instead of var
+    // https://eslint.org/docs/rules/no-var
     'no-var': ['error'],
+
+    // Require object literal shorthand syntax
+    // https://eslint.org/docs/rules/object-shorthand
     'object-shorthand': ['error', 'always'],
-    'prefer-arrow-callback': ['error'],
-    'prefer-const': ['error', { 'destructuring': 'any', 'ignoreReadBeforeAssign': false }],
-    'prefer-destructuring': ['off'],
-    'prefer-numeric-literals': ['off'],
-    'prefer-rest-params': ['off'],
-    'prefer-spread': ['off'],
+
+    // Require using arrow functions for callbacks
+    // https://eslint.org/docs/rules/prefer-arrow-callback
+    'prefer-arrow-callback': ['error', { allowNamedFunctions: false, allowUnboundThis: true }],
+
+    // Suggest using const
+    // https://eslint.org/docs/rules/prefer-const
+    'prefer-const': ['error', { destructuring: 'any', ignoreReadBeforeAssign: false }],
+
+    // Prefer destructuring from arrays and objects
+    // https://eslint.org/docs/rules/prefer-destructuring
+    'prefer-destructuring': ['error'],
+
+    // Disallow parseInt() and Number.parseInt() in favor of binary, octal, and hexadecimal literals
+    // https://eslint.org/docs/rules/prefer-numeric-literals
+    'prefer-numeric-literals': ['error'],
+
+    // Suggest using the rest parameters instead of arguments
+    // https://eslint.org/docs/rules/prefer-rest-params
+    'prefer-rest-params': ['error'],
+
+    // Suggest using spread syntax instead of .apply()
+    // https://eslint.org/docs/rules/prefer-spread
+    'prefer-spread': ['error'],
+
+    // Suggest using template literals instead of string concatenation
+    // https://eslint.org/docs/rules/prefer-template
     'prefer-template': ['error'],
+
+    // Disallow generator functions that do not have yield
+    // https://eslint.org/docs/rules/require-yield
     'require-yield': ['error'],
+
+    // Enforce spacing between rest and spread operators and their expressions
+    // https://eslint.org/docs/rules/rest-spread-spacing
     'rest-spread-spacing': ['error', 'never'],
+
+    // Import Sorting
+    // https://eslint.org/docs/rules/sort-imports
     'sort-imports': ['off'],
+
+    // Ρequire symbol description
+    // https://eslint.org/docs/rules/symbol-description
     'symbol-description': ['error'],
+
+    // Enforce usage of spacing in Template Strings
+    // https://eslint.org/docs/rules/template-curly-spacing
     'template-curly-spacing': ['error', 'never'],
-    'yield-star-spacing': ['error', { 'before': false, 'after': true }]
+
+    // Enforce spacing around the * in yield* expressions
+    // https://eslint.org/docs/rules/yield-star-spacing
+    'yield-star-spacing': ['error', { before: false, after: true }]
   }
 };
